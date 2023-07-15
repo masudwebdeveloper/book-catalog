d1bf0023b8ad8bb478a60c8e595b0cd76aa46768
masudrana5 <abdullainfo30@gmail.com>
2023-07-15 23:27:14 +0600

    onAuthStateChanged is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import MainLayout from "./layouts/MainLayout";
 import { Toaster } from "react-hot-toast";
+import { useAppDispatch } from "./redux/hook";
+import { useEffect } from "react";
+import { setLoading, setUser } from "./redux/features/user/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(setLoading(true));
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user?.email));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, [dispatch]);
   return (
     <div>
       <Toaster />
